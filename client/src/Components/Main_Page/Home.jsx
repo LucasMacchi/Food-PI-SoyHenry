b5bd68135d50e73abe8f8bd4590c8b5244f0cc3a
Lucas Macchi <lucasmacchi25@gmail.com>
2022-09-22 12:03:17 -0300

    ya casi terminado

--- client/src/Components/Main_Page/Home.jsx
+++ client/src/Components/Main_Page/Home.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import * as actions from "../../Redux/actions"
 import Card from "../Card/Card";
 import {useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import "../../Styles/Home.css"
 
-const ITEMS_PER_PAGE = 9
+const ITEMS_PER_PAGE = 9 //Este numero se encarga de limitar la cantidad de elementos que se pueden mostrar por pagina
 
 
 export default function Home () {
@@ -13,8 +13,8 @@ export default function Home () {
     const dietTypes = useSelector(state=> state.dietTypes)
     const recetasFiltradas = useSelector(state => state.filteredRecipes)
     const dispatch = useDispatch()
-
-    useEffect(() => {
+    //Este chequea si ya estan cargados los datos, para no hacerlo de nuevo
+    useEffect(() => { 
         if(recetas.length === 0){
             dispatch(actions.getAllRecipes())
         }
@@ -23,28 +23,26 @@ export default function Home () {
         }
         
     }, [])
-        //Estados
-        const [filtro, setFiltro] = React.useState({filtrado: []})
-        const [input, setInput] = React.useState({name:""})
-        const [order, setOrder] = React.useState({
+        //Estados que voy a usar para controlar el flow de la pagina
+        const [filtro, setFiltro] = React.useState({filtrado: []})  //Este para guardar cada arreglo que tenga que mostrar
+        const [input, setInput] = React.useState({name:""}) //Este guarda el nombre que voy a buscar
+        const [order, setOrder] = React.useState({  //Este estado me permite diferenciar el tipo de orden y el payload es el dato que voy a utilizar en la busqueda
             order:"none",
             payload:"none",
             
         })
-        const [page, setPage] = React.useState({current: 1,prev: 0, max: filtro.filtrado.length / 10})
-
-        useEffect(() => {
+        const [page, setPage] = React.useState({current: 1,prev: 0, max: filtro.filtrado.length / ITEMS_PER_PAGE})  //Me deja controlar el paginado
+        ///////////////////////////////
+        useEffect(() => {   //Este actualizara el filtrado cada vez que "recetas" (totas las recetas sin filtrar) se actualice, se usa en caso de ordenamientos
             setFiltro({filtrado: arrayDiveder(recetas)})
             setPage({...page,max: Math.round(recetas.length / ITEMS_PER_PAGE)})
         },[recetas])
-
+        //pasa exactamente los mismo aca
         useEffect(() => {
             setFiltro({filtrado: arrayDiveder(recetasFiltradas)})
             setPage({prev: 0,current: 1,max: Math.round(recetasFiltradas.length / ITEMS_PER_PAGE)})
         },[recetasFiltradas])
-        //////////
-    
-
+        //Este useEffect es super importante, me deja manejar el estado order cada vez que se actualiza, haciendo la logica correspondiente de cada orden
     useEffect(() => {
         console.log("ORDEN = "+order.order+" PAYLOAD = "+order.payload)
         if(order.payload === "none") {
@@ -122,7 +120,8 @@ export default function Home () {
             
         }
     },[order])
-
+    //Este me deja que cada vez que se actualiza la pagina, tengo que dividir devuelta el array de recetas para mostrar las siguientes
+    //esto depende obviamente si estoy viendo las recestas filtradas o todas las recetas
     useEffect(() => {
         console.log("pagina siguiente")
         if(order.order === "diet") setFiltro({filtrado: arrayDiveder(recetasFiltradas)})
@@ -132,13 +131,17 @@ export default function Home () {
         }
         
     },[page])
-
+    //Este me deja resetear la busqueda si no hay nada escrito en el buscador, haciendo que se muestren todas las recetas
     useEffect(() => {
         if(input.name === "") setOrder({...order, payload: "none"})
     },[input])
 
 
     //Handlers
+    //La logica de cambiar las paginas es ir cambiando el "slice" que se le hace al arreglo de recetas, se usa la pagina acutal multiplicado
+    //por la cantidad maxima de elementos para ir cambiando que hasta donde llega el slice y se usa el "prev" que se va sumando o restando 
+    //la cantidad maxima de elementos, esto me daria de donde empieza el slice del arreglo de recetas, todo esto me da una logica para ir moviendote 
+    //por un arreglo mientras solo mostrar los elementos de a 9 o de otro numero
     const nextPage = () => {
         setPage({...page,current: page.current+1, prev: page.prev+ITEMS_PER_PAGE})
         
@@ -158,7 +161,7 @@ export default function Home () {
     const handleOrdering = (e) => {
         setOrder({order: "ordering",payload: e})
     }
-
+    //Estos se usaran dentro del "sort" del arreglo de recetas para ordenar dependiendo de la orden enviada
     const comparadorHS_A = (a, b) => {
         if(a.healthScore == b.healthScore) return 0
         if(a.healthScore < b.healthScore) return -1
@@ -179,7 +182,7 @@ export default function Home () {
         b = b.name.toLowerCase()
         return b.localeCompare(a)
     }
-
+    //Maneja el input del nombre
     const formsHandler = (e) => {
         e.preventDefault()
         setOrder({order: "search",payload: input.name})
@@ -237,6 +240,7 @@ export default function Home () {
                 {createCardsFiltered() }
             </div>
             <div id="paginadoHome">
+                {/**Estos botones estan condicionados para no tener logica y no poder seguir o retorceder en caso de que no haya nada mas atras o adelante */}
                 {page.current === 1 ? <button disabled>{"<"}</button> : <button onClick={() => prevPage()}>{"<"}</button>}
                 <p>{page.current+" de "+page.max}</p>
                 {page.current === page.max ? <button disabled>{">"}</button> : <button  onClick={() => nextPage()}>{">"}</button>}
--- client/src/Components/Modified_rec/OwnRecipes.jsx
+++ client/src/Components/Modified_rec/OwnRecipes.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 //redux
 import {useDispatch, useSelector } from "react-redux";
 
